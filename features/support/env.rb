5474c40da19c4959ac970660d7cc35e88acd6f6d
Uldis Silins <uldis.silins@testdevlab.com>
2017-12-13 18:11:52 +0200

    Added an after hook

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,3 +2,7 @@ Before() do
   puts "Before hook. This will work before every test case!"
   @test_user = User.new('api_test_account@yopmail.com', 'Parole123')
 end
+
+After() do
+  puts "This happens after a test!"
+end
